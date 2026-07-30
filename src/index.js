278139b86a0f798805d0dbbb48749cb985242a0a
Pascal Baljet <pascal@protone.media>
2026-07-30 23:10:47 +0200

    Fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { nextTick, onMounted, watch } from 'vue'
 
+export const inertiaUiFontHead = [
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    [
+        'link',
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Albert+Sans:wght@100..900&family=Geist+Mono:wght@100..900&family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&family=Schibsted+Grotesk:wght@400..900&display=swap',
+        },
+    ],
+]
+
 export const inertiaUiMarkdownTheme = {
     light: {
         name: 'inertia-ui-light',
