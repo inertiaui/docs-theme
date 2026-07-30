22c524b2bf9fe59b86ce05a1b0b55632777fc2e0
Pascal Baljet <pascal@protone.media>
2026-07-30 23:08:22 +0200

    Markdown theme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
 import { nextTick, onMounted, watch } from 'vue'
 
+export const inertiaUiMarkdownTheme = {
+    light: {
+        name: 'inertia-ui-light',
+        type: 'light',
+        colors: {
+            'editor.background': '#f6f7fb',
+            'editor.foreground': '#111827',
+        },
+        tokenColors: [
+            { scope: ['comment', 'punctuation.definition.comment'], settings: { foreground: '#9ca3af', fontStyle: 'italic' } },
+            { scope: ['keyword', 'storage', 'storage.type', 'keyword.control', 'keyword.operator.new'], settings: { foreground: '#7e57c2', fontStyle: '' } },
+            { scope: ['string', 'string.quoted', 'punctuation.definition.string'], settings: { foreground: '#15803d' } },
+            { scope: ['constant.numeric', 'constant.language', 'constant.character'], settings: { foreground: '#c2410c' } },
+            { scope: ['variable', 'variable.other', 'variable.parameter', 'variable.language'], settings: { foreground: '#334155' } },
+            {
+                scope: ['support.function', 'entity.name.function', 'meta.function-call', 'meta.function-call entity.name.function'],
+                settings: { foreground: '#2563eb' },
+            },
+            { scope: ['entity.name.class', 'entity.name.type', 'support.class', 'entity.other.inherited-class'], settings: { foreground: '#5b21b6' } },
+            { scope: ['entity.name.tag', 'meta.tag'], settings: { foreground: '#2563eb' } },
+            { scope: ['entity.other.attribute-name'], settings: { foreground: '#7e57c2' } },
+            { scope: ['punctuation', 'meta.brace', 'punctuation.section'], settings: { foreground: '#64748b' } },
+            { scope: ['variable.other.property', 'support.type.property-name'], settings: { foreground: '#334155' } },
+            { scope: ['support.type', 'support.constant'], settings: { foreground: '#7e57c2' } },
+        ],
+    },
+    dark: {
+        name: 'inertia-ui-dark',
+        type: 'dark',
+        colors: {
+            'editor.background': '#1f1a2e',
+            'editor.foreground': '#e8e3ec',
+        },
+        tokenColors: [
+            { scope: ['comment', 'punctuation.definition.comment'], settings: { foreground: '#676e95', fontStyle: 'italic' } },
+            { scope: ['keyword', 'storage', 'storage.type', 'keyword.control', 'keyword.operator.new'], settings: { foreground: '#c792ea', fontStyle: '' } },
+            { scope: ['string', 'string.quoted', 'punctuation.definition.string'], settings: { foreground: '#c3e88d' } },
+            { scope: ['constant.numeric', 'constant.language', 'constant.character'], settings: { foreground: '#f78c6c' } },
+            { scope: ['variable', 'variable.other', 'variable.parameter', 'variable.language'], settings: { foreground: '#a6accd' } },
+            {
+                scope: ['support.function', 'entity.name.function', 'meta.function-call', 'meta.function-call entity.name.function'],
+                settings: { foreground: '#82aaff' },
+            },
+            { scope: ['entity.name.class', 'entity.name.type', 'support.class', 'entity.other.inherited-class'], settings: { foreground: '#ffcb6b' } },
+            { scope: ['entity.name.tag', 'meta.tag'], settings: { foreground: '#89ddff' } },
+            { scope: ['entity.other.attribute-name'], settings: { foreground: '#c792ea' } },
+            { scope: ['punctuation', 'meta.brace', 'punctuation.section'], settings: { foreground: '#89ddff' } },
+            { scope: ['variable.other.property', 'support.type.property-name'], settings: { foreground: '#a6accd' } },
+            { scope: ['support.type', 'support.constant'], settings: { foreground: '#c792ea' } },
+        ],
+    },
+}
+
 export function setupCodeGroupTabs(storageKey, route) {
     if (typeof window === 'undefined' || !storageKey || !route) {
         return
