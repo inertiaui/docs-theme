d4e54404e72d57aa84492c0af8cf14020d42f822
Pascal Baljet <pascal@protone.media>
2026-07-30 23:12:59 +0200

    Head + links

--- src/index.js
+++ src/index.js
@@ -12,6 +12,24 @@ export const inertiaUiFontHead = [
     ],
 ]
 
+export function inertiaUiHead(entries = []) {
+    const productionHead =
+        process.env.NODE_ENV === 'production'
+            ? [
+                  [
+                      'script',
+                      {
+                          async: '',
+                          src: 'https://analytics.ahrefs.com/analytics.js',
+                          'data-key': 'A06g5iU8TH9IWlCPkl0/OQ',
+                      },
+                  ],
+              ]
+            : []
+
+    return [...productionHead, ...entries, ...inertiaUiFontHead]
+}
+
 export const inertiaUiMarkdownTheme = {
     light: {
         name: 'inertia-ui-light',
@@ -63,6 +81,49 @@ export const inertiaUiMarkdownTheme = {
             { scope: ['support.type', 'support.constant'], settings: { foreground: '#c792ea' } },
         ],
     },
+}
+
+export function inertiaUiSocialLinks({ githubLink = 'https://github.com/inertiaui' } = {}) {
+    return [
+        { icon: 'github', link: githubLink },
+        { icon: 'twitter', link: 'https://twitter.com/pascalbaljet' },
+        {
+            icon: {
+                svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M407.8 294.7c-3.3-.4-6.7-.8-10-1.3c3.4 .4 6.7 .9 10 1.3zM288 227.1C261.9 176.4 190.9 81.9 124.9 35.3C61.6-9.4 37.5-1.7 21.6 5.5C3.3 13.8 0 41.9 0 58.4S9.1 194 15 213.9c19.5 65.7 89.1 87.9 153.2 80.7c3.3-.5 6.6-.9 10-1.4c-3.3 .5-6.6 1-10 1.4C74.3 308.6-9.1 342.8 100.3 464.5C220.6 589.1 265.1 437.8 288 361.1c22.9 76.7 49.2 222.5 185.6 103.4c102.4-103.4 28.1-156-65.8-169.9c-3.3-.4-6.7-.8-10-1.3c3.4 .4 6.7 .9 10 1.3c64.1 7.1 133.6-15.1 153.2-80.7C566.9 194 576 75 576 58.4s-3.3-44.7-21.6-52.9c-15.8-7.1-40-14.9-103.2 29.8C385.1 81.9 314.1 176.4 288 227.1z"/></svg>',
+            },
+            link: 'https://bsky.app/profile/pascalbaljet.bsky.social',
+        },
+        { icon: 'youtube', link: 'https://youtube.com/pascalbaljet' },
+    ]
+}
+
+export function createInertiaUiDocsConfig({ productSlug, githubLink } = {}) {
+    if (!productSlug) {
+        throw new Error('createInertiaUiDocsConfig expects a productSlug.')
+    }
+
+    const production = process.env.NODE_ENV === 'production'
+
+    return {
+        lang: 'en-US',
+        base: production ? `/${productSlug}/docs/` : null,
+        outDir: production ? `./dist/${productSlug}/docs` : './dist',
+        cleanUrls: production,
+        markdown: {
+            theme: inertiaUiMarkdownTheme,
+        },
+        themeConfig: {
+            logo: {
+                light: '/inertiaui-logo.svg',
+                dark: '/inertiaui-logo-white.svg',
+                alt: 'Inertia UI',
+            },
+            logoLink: production ? `/${productSlug}/docs/introduction` : '/introduction',
+            search: { provider: 'local' },
+            aside: false,
+            socialLinks: inertiaUiSocialLinks({ githubLink }),
+        },
+    }
 }
 
 export function setupCodeGroupTabs(storageKey, route) {
